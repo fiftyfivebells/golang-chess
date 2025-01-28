d9e356754e670c482fc736645a05c4a719b9c581
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-01-27 22:30:44 -0500

    Add some potentially useful bitboard constants

--- engine/bitboard.go
+++ engine/bitboard.go
@@ -4,6 +4,29 @@ import "fmt"
 
 type Bitboard uint64
 
+const (
+	FileA Bitboard = 0x8080808080808080 >> iota
+	FileB
+	FileC
+	FileD
+	FileE
+	FileF
+	FileG
+	FileH
+
+	Rank8 Bitboard = 0xff
+	Rank7 Bitboard = Rank8 << 8
+	Rank6 Bitboard = Rank7 << 8
+	Rank5 Bitboard = Rank6 << 8
+	Rank4 Bitboard = Rank5 << 8
+	Rank3 Bitboard = Rank4 << 8
+	Rank2 Bitboard = Rank3 << 8
+	Rank1 Bitboard = Rank2 << 8
+
+	EmptyBitboard = Bitboard(0)
+	FullBitboard  = Bitboard(0xffffffffffffffff)
+)
+
 var SquareMasks [64]Bitboard
 
 func (bb *Bitboard) setBitAtSquare(square Square) {
