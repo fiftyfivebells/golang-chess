b39ea5c9f024101c50b3d107e0ef9801736a69f4
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-02-17 21:15:59 -0500

    Add move tables for K/Q/B/R/N

--- engine/move_tables.go
+++ engine/move_tables.go
@@ -1,22 +1,39 @@
 package engine
 
+var Rays = [8][64]Bitboard{}
+
 var KnightMoves = [64]Bitboard{}
 var KingMoves = [64]Bitboard{}
+var BishopMoves = [64]Bitboard{}
+var RookMoves = [64]Bitboard{}
+var QueenMoves = [64]Bitboard{}
 
 const (
+	North int = iota
+	NorthEast
+	East
+	SouthEast
+	South
+	SouthWest
+	West
+	NorthWest
+
 	notFileAOrB = ^(FileA | FileB)
 	notFileHOrG = ^(FileH | FileG)
 )
 
 func InitializeMoveTables() {
 
-	for square := A1; square <= H8; square++ {
-		KnightMoves[square] = CreateKnightMovesForSquare(square)
-		KingMoves[square] = CreateKingMovesForSquare(square)
+	for square := H1; square <= A8; square++ {
+		KnightMoves[square] = createKnightMovesForSquare(square)
+		KingMoves[square] = createKingMovesForSquare(square)
+		BishopMoves[square] = createBishopMovesForSquare(square)
+		RookMoves[square] = createRookMovesForSquare(square)
+		QueenMoves[square] = BishopMoves[square] | RookMoves[square]
 	}
 }
 
-func CreateKingMovesForSquare(square Square) Bitboard {
+func createKingMovesForSquare(square Square) Bitboard {
 
 	startingSquare := SquareMasks[square]
 
@@ -32,7 +49,7 @@ func CreateKingMovesForSquare(square Square) Bitboard {
 	return north | northEast | east | southEast | south | southWest | west | northWest
 }
 
-func CreateKnightMovesForSquare(square Square) Bitboard {
+func createKnightMovesForSquare(square Square) Bitboard {
 	startingSquare := SquareMasks[square]
 
 	northNorthWest := startingSquare << 17 & ^FileH
@@ -48,4 +65,40 @@ func CreateKnightMovesForSquare(square Square) Bitboard {
 	southSouthWest := startingSquare >> 15 & ^FileH
 
 	return northNorthWest | northNorthEast | eastEastNorth | westWestNorth | southSouthEast | eastEastSouth | westWestSouth | southSouthWest
+}
+
+// createDiagonalMask takes in a square and returns the bitboard that masks the diagonal lines
+// (positive and negative) from that square. The formula for calculating this comes from this link:
+// https://www.chessprogramming.org/On_an_empty_Board#By_Calculation_3
+func createDiagonalMask(square Square) Bitboard {
+	diagonal := 56 - 8*(int(square)&7) - (int(square) & 56)
+	north := -diagonal & (diagonal >> 31)
+	south := diagonal & (-diagonal >> 31)
+
+	return Diagonal >> south << north
+}
+
+// createAntiDiagonalMask takes in a square and returns the bitboard that masks the antidiagonal lines
+// (positive and megative) from that square. The formula for calculating this comes from the link above.
+func createAntiDiagonalMask(square Square) Bitboard {
+	antiDiagonal := 8*(int(square)&7) - (int(square) & 56)
+	north := -antiDiagonal & (antiDiagonal >> 31)
+	south := antiDiagonal & (-antiDiagonal >> 31)
+
+	return AntiDiagonal >> south << north
+}
+
+func createBishopMovesForSquare(square Square) Bitboard {
+	diagonal := createDiagonalMask(square)
+	antiDiagonal := createAntiDiagonalMask(square)
+
+	return (diagonal | antiDiagonal) ^ SquareMasks[square]
+}
+
+func createRookMovesForSquare(square Square) Bitboard {
+	// the calculations here are taken from the same link as above
+	rank := Bitboard(0xff) << (square & 56)
+	file := Bitboard(0x0101010101010101) << (square & 7)
+
+	return (rank | file) ^ SquareMasks[square]
 }
