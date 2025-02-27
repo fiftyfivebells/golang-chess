1b685f67f323cbdf2de79d714e563cf6f8688e37
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-02-26 21:20:51 -0500

    Start implementation of bitboard move generator

--- engine/movegen_bitboard.go
+++ engine/movegen_bitboard.go
@@ -17,13 +17,51 @@ func (bmg BitboardMoveGenerator) GetMoves() []Move {
 }
 
 func (bmg *BitboardMoveGenerator) GenerateMoves(activeSide Color, enPassant Square) {
+	activePieces := bmg.board.GetAllPiecesByColor(activeSide)
+	targets := bmg.board.GetAllPiecesByColor(^activeSide)
 
+	for pieceType := Knight; pieceType < None; pieceType++ {
+		pieceBoard := bmg.board.pieces[activeSide][pieceType]
+
+		for pieceBoard != 0 {
+			square := pieceBoard.PopLSB()
+			bmg.generateMovesByPiece(pieceType, square, activePieces, targets)
+		}
+	}
+
+	bmg.generatePawnMoves(activeSide, enPassant)
+	bmg.generateCastlingMoves(activeSide)
 }
 
-func (bmg *BitboardMoveGenerator) generateMovesByPiece(pieceType PieceType, from Square) {
+func (bmg *BitboardMoveGenerator) generateMovesByPiece(pieceType PieceType, from Square, activePieces, targets Bitboard) {
+	var moves Bitboard
+	switch pieceType {
+	case Knight:
+		moves = (KnightMoves[from] & ^activePieces) & targets
+	case King:
+		moves = (KingMoves[from] & ^activePieces) & targets
+	}
 
+	bmg.createMovesFromBitboard(from, moves, targets, pieceType)
 }
+
+func (bmg *BitboardMoveGenerator) generatePawnMoves(activeSide Color, enPassant Square) {}
+
+func (bmg *BitboardMoveGenerator) generateCastlingMoves(activeSide Color) {}
 
 func (bmg *BitboardMoveGenerator) createMovesFromBitboard(from Square, moves, targets Bitboard, pieceType PieceType) {
 
+	for moves != 0 {
+		to := moves.PopLSB()
+		toBoard := SquareMasks[to]
+
+		moveType := Quiet
+
+		if (toBoard & targets) != 0 {
+			moveType = Capture
+		}
+
+		move := NewMove(from, to, pieceType, moveType)
+		bmg.moves = append(bmg.moves, move)
+	}
 }
